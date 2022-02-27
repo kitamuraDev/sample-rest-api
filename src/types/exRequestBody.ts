aa6cdbf298370a63bc3d869d07772286bb8858fa
kitamuraDev <masato1999824@icloud.com>
2022-02-27 18:09:58 +0900

    :recycle: req.body周りの型定義をリファクタリングした

--- src/types/exRequestBody.ts
+++ src/types/exRequestBody.ts
@@ -1,12 +1,20 @@
-type Bodys = 'name' | 'profile' | 'date_of_birth';
-export interface ExRequestBoby extends Express.Request {
+import { Request } from 'express';
+
+export interface ExRequestBody extends Request {
   body: {
-    [key in Bodys]: string;
+    [key in 'name' | 'profile' | 'date_of_birth']: string;
   };
 }
 
 /**
- * bad pattern "type alias"
+ * bad pattern ① "extends Express.Request"
+ *  - 正常にexpressのRequestが継承されない
+ *  - 【例】"req."をタイプした時点でbodyのみしか候補が出てこないし、無理やり"req.params"とタイプしても
+ *     "プロパティ 'params' は型 'ExRequestBody<Bodys>' に存在しません"とエラーが出る
+ */
+
+/**
+ * bad pattern ② "type alias"
  */
 // type ExRequestBoby = Request & {
 //   body: {
